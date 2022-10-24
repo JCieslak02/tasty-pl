c814d3a157809a32be743c97d3dfa7bd432e1516
JCieslak <cieslak1510@gmail.com>
2022-10-24 11:26:22 +0200

    test

---
+++ src/main/java/com/jcieslak/tastypl/TastyPlApplication.java
@@ -0,0 +1,13 @@
+package com.jcieslak.tastypl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyPlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyPlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcieslak/tastypl/TastyPlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcieslak.tastypl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyPlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
