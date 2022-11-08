6b5044e5cba5eb7aa22afa4eece9d86dedab0091
JCieslak <cieslak1510@gmail.com>
2022-11-08 13:32:34 +0100

    exception name change

--- src/main/java/com/jcieslak/tastypl/exception/HasNonUniqueFieldsException.java
+++ src/main/java/com/jcieslak/tastypl/exception/HasNonUniqueFieldsException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class HasDuplicateFieldsException extends RuntimeException{
-    public HasDuplicateFieldsException(String type) {
-        super(String.format("This %s has at least 1 duplicate field", type));
+public class HasNonUniqueFieldsException extends RuntimeException{
+    public HasNonUniqueFieldsException(String type) {
+        super(String.format("This %s has at least 1 non unique field", type));
     }
 }
