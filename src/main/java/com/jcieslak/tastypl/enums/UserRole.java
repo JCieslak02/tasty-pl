348ec08ddc5e74db6dcc61026e256c5d450a2b13
JCieslak <cieslak1510@gmail.com>
2022-12-29 13:00:28 +0100

    major codebase changes

---
+++ src/main/java/com/jcieslak/tastypl/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.jcieslak.tastypl.enums;
+
+public enum UserRole {
+    ROLE_CUSTOMER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/jcieslak/tastypl/model/User.java
@@ -0,0 +1,83 @@
+package com.jcieslak.tastypl.model;
+
+import com.jcieslak.tastypl.enums.UserRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Table(name= "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && role == user.role && Objects.equals(email, user.email) && Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, username, password, role, email, phoneNumber);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority(role.name())));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
