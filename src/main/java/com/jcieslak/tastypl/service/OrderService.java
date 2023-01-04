0f8039db34a8d9582bd6bf3dc1936f9e96358303
JCieslak <cieslak1510@gmail.com>
2023-01-04 17:37:50 +0100

    order placement implemented

--- src/main/java/com/jcieslak/tastypl/service/OrderService.java
+++ src/main/java/com/jcieslak/tastypl/service/OrderService.java
@@ -1,24 +1,26 @@
 package com.jcieslak.tastypl.service;
 
 import com.jcieslak.tastypl.enums.OrderStatus;
-import com.jcieslak.tastypl.model.Meal;
-import com.jcieslak.tastypl.model.Order;
-import com.jcieslak.tastypl.model.OrderMealQuantity;
-import com.jcieslak.tastypl.model.User;
+import com.jcieslak.tastypl.exception.MealIsFromDifferentRestaurantException;
+import com.jcieslak.tastypl.model.*;
+import com.jcieslak.tastypl.payload.request.MealQuantityRequest;
 import com.jcieslak.tastypl.payload.request.OrderRequest;
 import com.jcieslak.tastypl.payload.response.MealQuantityResponse;
 import com.jcieslak.tastypl.payload.response.OrderResponse;
-import com.jcieslak.tastypl.repository.MealRepository;
 import com.jcieslak.tastypl.repository.OrderRepository;
 import com.jcieslak.tastypl.security.config.UserDetailsServiceImpl;
+import com.jcieslak.tastypl.util.MealQuantityListMapper;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -26,15 +28,23 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final UserDetailsServiceImpl userDetailsService;
     private final ModelMapper modelMapper;
-    private final MealRepository mealRepository;
     private final MealService mealService;
+    private final AddressService addressService;
+    private final MealQuantityListMapper mealQuantityListMapper;
+    private final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
     public OrderResponse createOrder(OrderRequest orderRequest, Principal principal){
-        User user = userDetailsService.loadUserByUsername(principal.getName());
+        // this block of code ensures that there won't be any address duplicates in db - if it's present in db, it'll be set as provided address
+        // in order request
+        Address address = addressService.getDuplicateAddressOrProvided(orderRequest.getAddress());
+        orderRequest.setAddress(address);
         Order order = modelMapper.map(orderRequest, Order.class);
 
-        List<OrderMealQuantity> orderMealQuantityList = orderRequest
-                .getOrderMealQuantities().stream()
+        // creating a list of OrderMealQuantity for later counting total price and saving it to db via cascade
+        // by setting order it as a field in order entity
+        List<MealQuantityRequest> mealQuantityRequestList = orderRequest.getMealQuantityRequestList();
+
+        List<OrderMealQuantity> orderMealQuantityList = mealQuantityRequestList.stream()
                 .map(o -> {
                     OrderMealQuantity orderMealQuantity = modelMapper.map(o, OrderMealQuantity.class);
                     orderMealQuantity.setMeal(mealService.getMealByIdOrThrowExc(o.getMealId()));
@@ -43,36 +53,73 @@ public class OrderService {
                 })
                 .toList();
 
-        BigDecimal total = orderMealQuantityList.stream()
-                .map(orderMealQuantity -> {
-                    Meal meal = orderMealQuantity.getMeal();
-                    int quantity = orderMealQuantity.getQuantity();
+        // check whether all meals are from the same restaurant, throw exception if not
+        validateMealsRestaurant(orderMealQuantityList, orderRequest);
+
+        // calculate total price
+        BigDecimal total = calculateTotal(orderMealQuantityList);
+
+        // property settings to save order in db together with address and OrderMealQuantity
+        User user = userDetailsService.loadUserByUsername(principal.getName());
+        order.setUser(user);
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderMealQuantityList(orderMealQuantityList);
+        order.setTotal(total);
+        order.setDateTime(LocalDateTime.now());
+
+        Order savedOrder = orderRepository.save(order);
+
+        // lines below convert stuff to create OrderResponse return object
+        List<MealQuantityResponse> mealQuantityResponseList = mealQuantityListMapper.toResponse(orderMealQuantityList);
+
+        OrderResponse orderResponse = modelMapper.map(savedOrder, OrderResponse.class);
+        orderResponse.setMealQuantityResponseList(mealQuantityResponseList);
+        logger.info("Order created successfully");
+        return orderResponse;
+    }
+
+    public List<OrderResponse> getAllOrdersByRestaurantId(Long restaurantId){
+        List<Order> orderList = orderRepository.findAllByRestaurantId(restaurantId);
+
+        return getOrderResponses(orderList);
+    }
+
+    public List<OrderResponse> getAllOrdersByUserId(Long userId){
+        List<Order> orderList = orderRepository.findAllByUserId(userId);
+
+        return getOrderResponses(orderList);
+    }
+
+    //method used internally to create a list of OrderResponses from a list of Orders from repo
+    public List<OrderResponse> getOrderResponses(List<Order> orderList){
+        return orderList.stream()
+                .map(o -> {
+                    OrderResponse orderResponse = modelMapper.map(o, OrderResponse.class);
+                    orderResponse.setMealQuantityResponseList(mealQuantityListMapper.toResponse(o.getOrderMealQuantityList()));
+                    return orderResponse;
+                })
+                .toList();
+    }
+
+    //method checks if all meals are from the same restaurant, throws exception if not
+    public void validateMealsRestaurant(List<OrderMealQuantity> orderMealQuantityList, OrderRequest orderRequest){
+        boolean isAnyMealFromDifferentRestaurant = orderMealQuantityList.stream()
+                .anyMatch(m -> !Objects.equals(m.getMeal().getRestaurant().getId(), orderRequest.getRestaurantId()));
+
+        if(isAnyMealFromDifferentRestaurant){
+            throw new MealIsFromDifferentRestaurantException();
+        }
+    }
+
+    //method calculates total price for an order
+    public BigDecimal calculateTotal(List<OrderMealQuantity> orderMealQuantityList){
+        return orderMealQuantityList.stream()
+                .map(o -> {
+                    Meal meal = o.getMeal();
+                    int quantity = o.getQuantity();
                     BigDecimal price = meal.getPrice();
                     return price.multiply(BigDecimal.valueOf(quantity));
                 })
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        order.setUser(user);
-        order.setOrderStatus(OrderStatus.PENDING);
-        order.setMeals(orderMealQuantityList);
-        order.setTotal(total);
-        order.setDateTime(LocalDateTime.now());
-
-        orderRepository.save(order);
-
-        List<MealQuantityResponse> mealQuantityResponseList = orderMealQuantityList.stream()
-                .map(o -> {
-                    MealQuantityResponse m = new MealQuantityResponse();
-                    m.setMealId(o.getMeal().getId());
-                    m.setName(o.getMeal().getName());
-                    m.setType(o.getMeal().getType());
-                    m.setPrice(o.getMeal().getPrice());
-                    return m;
-                })
-                .toList();
-
-        OrderResponse orderResponse = modelMapper.map(order, OrderResponse.class);
-        orderResponse.setMeals(mealQuantityResponseList);
-        return orderResponse;
     }
 }
