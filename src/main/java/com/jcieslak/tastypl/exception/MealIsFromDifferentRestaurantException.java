6181bde1defcb9dfef90e311eaf2a4d54bb9c434
JCieslak <cieslak1510@gmail.com>
2023-01-04 17:36:38 +0100

    new exception added

---
+++ src/main/java/com/jcieslak/tastypl/exception/MealIsFromDifferentRestaurantException.java
@@ -0,0 +1,11 @@
+package com.jcieslak.tastypl.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class MealIsFromDifferentRestaurantException extends RuntimeException{
+    public MealIsFromDifferentRestaurantException() {
+        super("Provided meal with id %s is from another restaurant");
+    }
+}
