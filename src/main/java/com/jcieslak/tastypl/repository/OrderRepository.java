f4007b0a180c52eaac69df17babc151839944207
JCieslak <cieslak1510@gmail.com>
2023-01-04 17:36:49 +0100

    repo changes

--- src/main/java/com/jcieslak/tastypl/repository/OrderRepository.java
+++ src/main/java/com/jcieslak/tastypl/repository/OrderRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByRestaurantId(Long id);
+    List<Order> findAllByUserId(Long id);
 }
