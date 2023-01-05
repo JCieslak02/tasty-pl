a09f81dbc00f7c69da909c43ca9fc5358a0d8961
JCieslak <cieslak1510@gmail.com>
2023-01-05 17:39:15 +0100

    exception for enum

---
+++ src/main/java/com/jcieslak/tastypl/exception/EnumConstantNotPresentException.java
@@ -0,0 +1,11 @@
+package com.jcieslak.tastypl.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class EnumConstantNotPresentException extends RuntimeException{
+    public EnumConstantNotPresentException(String constant) {
+        super(String.format("Constant %s is not a constant", constant));
+    }
+}
