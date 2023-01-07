fc8c3dfed1c6b71d5f6fa3c2791390785da0d842
JCieslak <cieslak1510@gmail.com>
2023-01-07 23:58:02 +0100

    new request

---
+++ src/main/java/com/jcieslak/tastypl/payload/request/OrderStatusUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.jcieslak.tastypl.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderStatusUpdateRequest {
+    private String orderStatus;
+}
