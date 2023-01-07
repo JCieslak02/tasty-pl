048100d95b8d36c3a9b1df83e616efa9df5de14a
JCieslak <cieslak1510@gmail.com>
2023-01-07 21:44:43 +0100

    Added id to MealResponse

--- src/main/java/com/jcieslak/tastypl/payload/response/MealResponse.java
+++ src/main/java/com/jcieslak/tastypl/payload/response/MealResponse.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MealResponse {
+    private Long id;
     private String name;
     private String type;
     private BigDecimal price;
