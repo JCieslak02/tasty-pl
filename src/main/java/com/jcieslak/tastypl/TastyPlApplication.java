a9d4a58caccc552cb858da8e38089b34e19e3e07
JCieslak <cieslak1510@gmail.com>
2023-01-22 18:35:13 +0100

    moved ModelMapper bean to a proper class

--- src/main/java/com/jcieslak/tastypl/TastyPlApplication.java
+++ src/main/java/com/jcieslak/tastypl/TastyPlApplication.java
@@ -1,18 +1,11 @@
 package com.jcieslak.tastypl;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TastyPlApplication {
     public static void main(String[] args) {
         SpringApplication.run(TastyPlApplication.class, args);
     }
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
